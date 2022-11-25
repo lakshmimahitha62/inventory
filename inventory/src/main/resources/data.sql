7aa56f5f0e8297677d172df2777e91f59a4a55bd
2116702 <2116702@5CG01599SV.cts.com>
2022-11-25 17:06:22 +0530

    c

--- inventory/src/main/resources/data.sql
+++ inventory/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into users values("Mahi1234",'2008-11-11',"mahita@gmail.com","mahitha","18932983928","admin");
-insert into login values("Mahi1234","mahitha");
+insert into users values('Mahi1234','2008-11-11','mahita@gmail.com','mahitha','18932983928','admin');
+insert into login values('Mahi1234','mahitha');
